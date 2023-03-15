14e4432898b170c4769ad40e7bc2e42eb2110ffe
VerrryGood <98000496+VerrryGood@users.noreply.github.com>
2023-03-15 17:12:35 +0900

    제작중...

--- LCDTester/SendClient.cs
+++ LCDTester/SendClient.cs
@@ -4,12 +4,21 @@ using System.Linq;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using Timers = System.Timers;
 using System.Threading.Tasks;
 
 namespace LCDTester
 {
     public class SendClient
     {
+        private UdpClient sendClient;
+        private IPEndPoint serverEndPoint;
+        private int floorPort;
 
+        public SendClient(int port)
+        {
+            floorPort = port;
+            serverEndPoint = new IPEndPoint(IPAddress.Any, floorPort);
+        }
     }
 }
